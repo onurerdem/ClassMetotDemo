77a779c3af4e0661f68f1ca9f2b37912fb57fd6c
Onur Erdem <erdem.onur@outlook.com.tr>
2021-02-16 01:41:30 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkleme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi: " + musteri.MusteriAdi + " " + musteri.MusteriSoyadi + "\n");
+        }
+        public void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi: " + musteri.MusteriAdi + " " + musteri.MusteriSoyadi + "\n");
+        }
+        public void MusteriListeleme(Musteri[] musteriler)
+        {
+            //Console.WriteLine("Musteriler: " + musteri.MusteriId + " " + musteri.MusteriAdi + " " + musteri.MusteriSoyadi + " " + musteri.MusteriAdresi + " " + musteri.MusteriTel + " " + musteri.MusteriBakiyesi + "TL");
+            
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Müşteri Id: " + musteri.MusteriId);
+                Console.WriteLine("Müşteri Adı: " + musteri.MusteriAdi);
+                Console.WriteLine("Müşteri Soyadı: " + musteri.MusteriSoyadi);
+                Console.WriteLine("Müşteri Adresi: " + musteri.MusteriAdresi);
+                Console.WriteLine("Müşteri Telefonu: " + musteri.MusteriTel);
+                Console.WriteLine("Müşteri Bakiyesi: " + musteri.MusteriBakiyesi + " TL \n");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriId = 1;
+            musteri1.MusteriAdi = "Onur";
+            musteri1.MusteriSoyadi = "Erdem";
+            musteri1.MusteriAdresi = "İstanbul";
+            musteri1.MusteriTel = "1234";
+            musteri1.MusteriBakiyesi = "10";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriId = 2;
+            musteri2.MusteriAdi = "Engin";
+            musteri2.MusteriSoyadi = "Demiroğ";
+            musteri2.MusteriAdresi = "İstanbul";
+            musteri2.MusteriTel = "4321";
+            musteri2.MusteriBakiyesi = "2000";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.MusteriId = 3;
+            musteri3.MusteriAdi = "Mehmet";
+            musteri3.MusteriSoyadi = "Yılmaz";
+            musteri3.MusteriAdresi = "Ankara";
+            musteri3.MusteriTel = "2341";
+            musteri3.MusteriBakiyesi = "100";
+
+            Musteri musteri4 = new Musteri();
+            musteri4.MusteriId = 4;
+            musteri4.MusteriAdi = "Ahmet";
+            musteri4.MusteriSoyadi = "Öztürk";
+            musteri4.MusteriAdresi = "Ankara";
+            musteri4.MusteriTel = "3412";
+            musteri4.MusteriBakiyesi = "200";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3, musteri4 };
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkleme(musteri1);
+            musteriManager.MusteriSilme(musteri1);
+            musteriManager.MusteriListeleme(musteriler);
+        }
+    }
+}
